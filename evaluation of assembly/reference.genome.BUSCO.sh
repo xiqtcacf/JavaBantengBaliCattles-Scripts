bce578279a9864394dcafd7c5d857cef80a9dde3
XiWang <47152099+xiqtcacf@users.noreply.github.com>
2026-01-19 00:32:05 +0100

    Add files via upload

---
+++ evaluation of assembly/reference.genome.BUSCO.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export DIR=/home/wlk579/1.0JavaBanteng_project/0.Reference_Banteng_BosTau9_Waterbuffol_comparison/Banteng_PlusMt
+cd $DIR
+
+batch=$1
+
+###### evaluate quality of assembly, using quast5.0.2 old analyses
+#./quast.py /isdata/hellergrp/wlk579/reference_Bos/Bubalus_bubalis_WaterBuffalo_GCF_003121395.1_refseq_2018/GCF_003121395.1_ASM312139v1_genomic.fna.gz -o /isdata/hellergrp/wlk579/reference_comparison/waterBuffalo -L --gene-finding --eukaryote --split-scaffolds
+
+###### evaluate quality of assembly and annotation, using BUSCO-4.5.5
+###evaluate assembly quality by version BUSCO 4.5.5
+run_BUSCO=/home/wlk579/Server_bos/apps/mamba/install/envs/busco5/lib/python3.9/site-packages/busco/run_BUSCO.py
+#conda activate busco5
+
+#python3 $run_BUSCO -m genome -i $batch.fasta -o $batch.eukaryota -l eukaryota_odb10 -c 20
+python3 $run_BUSCO -m genome -i $batch.fasta -o $batch.cetartiodactyla -l cetartiodactyla_odb10 -c 20
+
+###evaluate annotation gene sets on kelly service by the newest version BUSCO 5.5.5
+#busco -m protein -i water_buffalo_GCF_003121395.1_ASM312139v1_protein.faa -o test.water_buffalo -l eukaryota_odb10
+#busco -m protein -i water_buffalo_GCF_003121395.1_ASM312139v1_protein.faa -o test.water_buffalo -l cetartiodactyla_odb10
+
+### draw figures
+plot=/home/wlk579/Server_bos/apps/mamba/install/envs/busco5/bin/generate_plot.py
+
+python3 $plot -wd $batch.eukaryota
