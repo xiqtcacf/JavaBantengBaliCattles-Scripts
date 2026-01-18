ea516a4f4313706c172f96977be8ec1a128ae8fc
XiWang <47152099+xiqtcacf@users.noreply.github.com>
2026-01-18 21:46:58 +0100

    Update Repeatmasker.getNONrepeat.bed.sh

--- Sites filtering/Repeatmasker.getNONrepeat.bed.sh
+++ Sites filtering/Repeatmasker.getNONrepeat.bed.sh
@@ -2,5 +2,4 @@ python3 XiaodongLiuWritten.extSoftRepeatFA.py Banteng_PlusMt.draft.fasta.masked 
 /home/users/xi/software/bedtools2/bin/bedtools subtract -a DefassaWaterbuck.Ref.scaffolds.all.bed -b DefassaWaterbuck.fasta.masked.repeat.bed > DefassaWaterbuck.fasta.masked.NOrepeat.bed
 
 python3 XiaodongLiuWritten.extSoftRepeatFA.py water_buffalo.reference.fna.masked > water_buffalo.reference.fna.masked.repeat.bed
-/home/users/xi/software/bedtools2/bin/bedtools subtract -a water_buffalo.reference.fna.fai.bed -b water_buffalo.reference.fna.masked.repeat.bed > 
-water_buffalo.reference.fna.masked.NOrepeat.bed
+/home/users/xi/software/bedtools2/bin/bedtools subtract -a water_buffalo.reference.fna.fai.bed -b water_buffalo.reference.fna.masked.repeat.bed > water_buffalo.reference.fna.masked.NOrepeat.bed
