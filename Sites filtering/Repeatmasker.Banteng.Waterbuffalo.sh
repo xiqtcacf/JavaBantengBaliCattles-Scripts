524b38eb4bb7cdc63b3a1ea4a5f58e7004f0e7ad
XiWang <47152099+xiqtcacf@users.noreply.github.com>
2026-01-18 21:46:13 +0100

    Update Repeatmasker.Banteng.Waterbuffalo.sh

--- Sites filtering/Repeatmasker.Banteng.Waterbuffalo.sh
+++ Sites filtering/Repeatmasker.Banteng.Waterbuffalo.sh
@@ -4,5 +4,5 @@ RepeatMasker  -pa 40  -frag 60000 -species Mammalia /home/users/xi/Waterbuck_pro
 
 
 RepeatMasker  -pa 40  -frag 60000 -species Mammalia /home/wlk579/Server_bos/data/reference/water_buffalo.reference.fna \
-                                  -dir /home/wlk579/2.0Indonesia_Bos_project/Indonesia_Bos_project/Good_sites_Waterbuffalo -xm -xsmall -gff \
-                                  2>&1 > /home/wlk579/2.0Indonesia_Bos_project/Indonesia_Bos_project/Good_sites_Waterbuffalo/water_buffalo.reference.fna.log
+                  -dir /home/wlk579/2.0Indonesia_Bos_project/Indonesia_Bos_project/Good_sites_Waterbuffalo -xm -xsmall -gff \
+                  2>&1 > /home/wlk579/2.0Indonesia_Bos_project/Indonesia_Bos_project/Good_sites_Waterbuffalo/water_buffalo.reference.fna.log
