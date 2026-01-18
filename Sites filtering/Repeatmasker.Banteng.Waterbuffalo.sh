2375ef470481d70f381c9ca8ad6e4a2392d84608
XiWang <47152099+xiqtcacf@users.noreply.github.com>
2026-01-18 21:45:36 +0100

    Add files via upload

---
+++ Sites filtering/Repeatmasker.Banteng.Waterbuffalo.sh
@@ -0,0 +1,8 @@
+RepeatMasker  -pa 40  -frag 60000 -species Mammalia /home/users/xi/Waterbuck_project/4.sites_filtering/1.repeats/Java_Banteng_Pro/Banteng_PlusMt.draft.fasta \
+				  -dir /home/users/xi/Waterbuck_project/4.sites_filtering/1.repeats/Java_Banteng_Pro -xm -xsmall -gff \
+				  2>&1 > /home/users/xi/Waterbuck_project/4.sites_filtering/1.repeats/Java_Banteng_Pro/Banteng_PlusMt.draft.fasta.log
+
+
+RepeatMasker  -pa 40  -frag 60000 -species Mammalia /home/wlk579/Server_bos/data/reference/water_buffalo.reference.fna \
+                                  -dir /home/wlk579/2.0Indonesia_Bos_project/Indonesia_Bos_project/Good_sites_Waterbuffalo -xm -xsmall -gff \
+                                  2>&1 > /home/wlk579/2.0Indonesia_Bos_project/Indonesia_Bos_project/Good_sites_Waterbuffalo/water_buffalo.reference.fna.log
