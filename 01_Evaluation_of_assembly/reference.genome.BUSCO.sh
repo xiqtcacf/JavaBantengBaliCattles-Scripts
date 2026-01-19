4a1d8afce5a69e8e9df7073bc57862c74b8b8ca0
XiWang <47152099+xiqtcacf@users.noreply.github.com>
2026-01-19 12:46:45 +0100

    Update reference.genome.BUSCO.sh

--- 01_Evaluation_of_assembly/reference.genome.BUSCO.sh
+++ 01_Evaluation_of_assembly/reference.genome.BUSCO.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DIR=/home/wlk579/1.0JavaBanteng_project/0.Reference_Banteng_BosTau9_Waterbuffol_comparison/Banteng_PlusMt
+export DIR=/home/wlk579/1.0JavaBanteng_project/0.Reference_Banteng_BosTau9_Waterbuffol_comparison/
 cd $DIR
 
 batch=$1
@@ -8,8 +8,8 @@ batch=$1
 ###### evaluate quality of assembly, using quast5.0.2 old analyses
 #./quast.py /isdata/hellergrp/wlk579/reference_Bos/Bubalus_bubalis_WaterBuffalo_GCF_003121395.1_refseq_2018/GCF_003121395.1_ASM312139v1_genomic.fna.gz -o /isdata/hellergrp/wlk579/reference_comparison/waterBuffalo -L --gene-finding --eukaryote --split-scaffolds
 
-###### evaluate quality of assembly and annotation, using BUSCO-4.5.5
-###evaluate assembly quality by version BUSCO 4.5.5
+###### evaluate quality of assembly, using BUSCO-4.5.5
+
 run_BUSCO=/home/wlk579/Server_bos/apps/mamba/install/envs/busco5/lib/python3.9/site-packages/busco/run_BUSCO.py
 #conda activate busco5
 
